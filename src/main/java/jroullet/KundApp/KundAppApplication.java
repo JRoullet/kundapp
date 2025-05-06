4c5cd54d98f362b1200c76f3c83dfb25ce7fea64
Jacques <jacques.roullet@laposte.net>
2025-05-06 10:40:54 +0200

    First commit : init

---
+++ src/main/java/jroullet/KundApp/KundAppApplication.java
@@ -0,0 +1,13 @@
+package jroullet.KundApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KundAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KundAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jroullet/KundApp/KundAppApplicationTests.java
@@ -0,0 +1,13 @@
+package jroullet.KundApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KundAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
